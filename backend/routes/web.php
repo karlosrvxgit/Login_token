215ec024c252329e384cc3fad1613ee1a15ba579
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-12-15 19:40:31 -0500

    updates crud

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UsersController::class, 'index']);
+Route::post('/users', [UsersController::class, 'store']);
+Route::put('/users/{id}', [UsersController::class, 'update']);
+Route::delete('/users/{id}', [UsersController::class, 'delete']);
